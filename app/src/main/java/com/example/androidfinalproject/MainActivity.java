d7ca23d4eb293a4544a54b8255f548891432e5aa
Will Foy <wfoy@andrew.cmu.edu>
2019-12-09 01:59:32 -0500

    facebook login working

--- app/src/main/java/com/example/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.androidfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mUsername;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    public static final String ANONYMOUS = "anonymous";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //AppEventsLogger.activateApp(getApplication());
+        mUsername = ANONYMOUS;
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, signinActivity.class));
+            finish();
+            Toast.makeText(getApplicationContext(),"USER SIGNED IN 1",Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+            Toast.makeText(getApplicationContext(),"USER SIGNED IN 2 as " + mUsername,Toast.LENGTH_SHORT).show();
+        }
     }
 }
